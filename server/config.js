22795f3737fbdfb39920a0e16356b0259eade6e4
thanhdo3044 <phamthanhdo18@gmail.com>
2023-03-07 14:53:03 +0700

    save 2

--- server/config.js
+++ server/config.js
@@ -1,7 +1,52 @@
-var admin = require("firebase-admin");
+'use strict';
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+
+const dotenv = require('dotenv');
+const assert = require('assert');
+
+dotenv.config();
+
+const {
+  PORT,
+  HOST,
+  HOST_URL,
+  API_KEY,
+  PROJECT_ID,
+  AUTH_DOMAIN,
+  MESSAGING_SEBDER_ID,
+  STORAGE_BUCKET,
+  MEASUREMENT_ID,
+  APP_ID,
+} = process.env;
+
+
+assert(PORT,'PORT is required');
+assert(HOST,'HOST is required');
+
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+module.exports={
+  port:PORT,
+  host:HOST,
+  url:HOST_URL,
+  firebaseConfig : {
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SEBDER_ID,
+    appId: APP_ID,
+    measurementId: MEASUREMENT_ID,
+  },
+}
+
+
+// Initialize Firebase
